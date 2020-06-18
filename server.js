730d87e21cd40d2ed1055a6dc70e06151517c1d6
Andrew Serra <andy@serra.us>
2020-06-18 14:39:45 +0300

    mongodb connects to droplet

--- server.js
+++ server.js
@@ -1,25 +1,27 @@
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const express = require('express');
+const dotenv = require('dotenv');
+const MongoClient = require('mongodb').MongoClient;
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 const PORT = 3000;
-const client = require('./dev/client');
 
-app.get('/', async (req, res) => {
-    client.connect()
-    .then(res => console.log(res))
-    .then(client => client.query("SELECT * FROM business"))
-    .then((r) => {
-        client.end();
-        res.send({res: r.rows})
-    })
-    .catch((err) => res.send({err: err}))
+// It will connect to database test
+// Later it will be changed to production when needed
+MongoClient.connect(process.env.MONGO_URI, function (err, db)  {
+    if(err) {
+        console.log(err);
+    }
+    else {
+        console.log('Connected to MongoDB.');
+    }
 })
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`)); 
