bf2825c5d2719fca77d4652c1142b9b40c07272d
Andrew Serra <andy@serra.us>
2020-06-19 19:09:22 +0300

    status codes as a collection for eliminating mistakes

---
+++ src/v1/utils/status_codes.js
@@ -0,0 +1,6 @@
+module.exports = {
+    SUCCESS: 200,       // Successful operation
+    NOT_FOUND: 301,     // Document not found
+    ERROR: 500,         // Any kind of error occuring on the server
+    BAD_REQUEST: 400,   // Missing data, wrong format in data (client errors)
+}
