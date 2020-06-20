b9ec0cb2b6dffbad28c945291428b2c0e26fa56c
Andrew Serra <andy@serra.us>
2020-06-20 15:59:08 +0300

    get specific business route

--- src/v1/routes/businesses.js
+++ src/v1/routes/businesses.js
@@ -1,4 +1,5 @@
 const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectID;
 const checkers = require('../utils/entry_checker');
 const bcrypt = require('bcrypt');
 const express = require('express');
@@ -9,7 +10,7 @@ const options = require('../utils/dbConnectionOptions');
 router = express.Router();
 
 // TODO:
-// 1- Write tests for endpoints and test for POST /
+// 1- Write tests for endpoints for POST /
 
 // Create a business for the first time
 router.post('/', function(req, res, next) {
@@ -74,6 +75,38 @@ router.post('/', function(req, res, next) {
     else {
         res.status(status_codes.BAD_REQUEST).send(check_result.response);
     }
+})
+
+// Get specific business document
+router.get('/:businessId', function(req, res) {
+    MongoClient.connect(process.env.MONGO_URI, options, function(error, client) {
+        if(error) {
+            res.status(status_codes.ERROR).send(error);
+            next();
+        }
+
+        // Connect to database, get collection
+        const db = client.db('dev_test');
+        const collection = db.collection("business");
+        
+        // Excluding fields
+        const options = {
+            projection: {
+                password: 0,
+                created: 0,
+            }
+        }
+        collection.findOne({ _id: ObjectId(req.params.businessId) }, options, function(error, doc) {
+            if(error) {
+                res.status(status_codes.ERROR).send(error);
+                client.close();
+                next();
+            }
+            if(doc) {
+                res.status(status_codes.SUCCESS).send(doc);
+            }
+        })
+    })
 })
 
 module.exports = router;
