a839df824de339dcbe80ef190fb2300f2eb5897f
Andrew Serra <andy@serra.us>
2020-06-23 17:26:15 +0300

    added the employee router to main.js for v1

--- src/v1/routes/main.js
+++ src/v1/routes/main.js
@@ -3,8 +3,10 @@ router = express.Router();
 
 const businessRouter = require('./businesses');
 const userRouter = require('./users');
+const employeeRouter = require('./employees');
 
 router.use('/businesses', businessRouter);
 router.use('/users', userRouter);
+router.use('/employees', employeeRouter);
 
 module.exports = router;
